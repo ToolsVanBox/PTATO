403bfaaad0293e21cf91077f1a38d64cfd2a6ab1
mroosmalen <m.j.vanroosmalen-3@prinsesmaximacentrum.nl>
2022-11-23 13:20:13 +0100

    Update for release

--- start_pipeline.sh
+++ start_pipeline.sh
@@ -3,10 +3,6 @@
 run_config_file=$1
 projectDir=$(dirname "$0")
 
-source ${projectDir}/scripts/bash/params_check.sh
-
-check_params ${projectDir} ${run_config_file}
-
 nextflow run \
 ${projectDir}/ptato.nf \
 -c ${run_config_file} \
